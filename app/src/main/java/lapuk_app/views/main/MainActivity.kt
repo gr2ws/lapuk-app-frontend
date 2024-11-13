72af3feda5a1dd0282f8f1d9402127ad6075a57e
gr2ws <gianasunan@gmail.com>
2024-11-14 01:27:20 +0800

    boilerplate main page

--- app/src/main/java/lapuk_app/views/main/MainActivity.kt
+++ app/src/main/java/lapuk_app/views/main/MainActivity.kt
@@ -7,12 +7,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
+import lapuk_app.views.main.ui.elements.ShowHeader
 import lapuk_app.views.main.ui.theme.LapukTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +29,6 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize()
                             .padding(innerPadding)
                     ) {
-                        Greeting("Android", Modifier.align(Alignment.Center))
                     }
                 }
             }
@@ -37,19 +36,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPreview() {
     LapukTheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            ShowHeader(modifier = Modifier.padding(innerPadding))
+        }
     }
 }
 
---
+++ app/src/main/java/lapuk_app/views/main/ui/elements/Logo.kt
@@ -0,0 +1,39 @@
+package lapuk_app.views.main.ui.elements
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.lapuk_app.R
+
+@Composable
+fun ShowLogo(color: Color, withText: Boolean) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (withText) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_w_name),
+                contentDescription = "logo",
+                colorFilter = ColorFilter.tint(color)
+            )
+        } else {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "logo",
+                colorFilter = ColorFilter.tint(color),
+                modifier = Modifier.size(40.dp) // Adjust the size as needed
+            )
+        }
+    }
+}
+
+
