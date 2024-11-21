fdc4e04d591e7606b662c5803fb27c8a727d1931
gr2ws <gianasunan@gmail.com>
2024-11-21 12:43:53 +0800

    resize overlay buttons

--- app/src/main/java/lapuk_app/views/main/ui/pages/Segregate.kt
+++ app/src/main/java/lapuk_app/views/main/ui/pages/Segregate.kt
@@ -78,7 +78,7 @@ fun SegregatePage(navController: NavController) {
         ) {
             IconButton(modifier = Modifier
                 .size(70.dp)
-                .border(3.dp, br5, shape = RoundedCornerShape(20.dp))
+                .border(4.dp, br5, shape = RoundedCornerShape(20.dp))
                 .shadow(elevation = 2.dp, shape = RoundedCornerShape(20.dp))
                 .background(br3, shape = RoundedCornerShape(20.dp)), onClick = {
                 navController.navigate("segregate/take-image") {
