d7853d4500ade49e8de57f8d3ae2c66f720db481
Lanz Alexander Malto <163155261+8-MrAlex-8@users.noreply.github.com>
2024-11-25 01:46:20 +0800

    Created file for Heatmaps functionality. TODO tomorrow.

---
+++ app/src/main/java/lapuk_app/views/main/ui/pages/Heatmaps.kt
@@ -0,0 +1,11 @@
+package lapuk_app.views.main.ui.pages
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun HeatmapsPage() {
+    Text("Hello World!")
+}
