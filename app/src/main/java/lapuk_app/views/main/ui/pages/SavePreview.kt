efb5b695e99a70c72e1e6fa2e5bb2b6adb9ba2ee
Gian Ross Wennette Asunan <gianasunan@gmail.com>
2024-11-25 23:52:00 +0800

    disable save button when analysis fails

--- app/src/main/java/lapuk_app/views/main/ui/pages/SavePreview.kt
+++ app/src/main/java/lapuk_app/views/main/ui/pages/SavePreview.kt
@@ -37,109 +37,131 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.withTimeout
 import lapuk_app.views.main.decodeToBitmap
 import lapuk_app.views.main.encodeBitmap
 import lapuk_app.views.main.requestAnalysis
 import lapuk_app.views.main.ui.theme.br1
+import lapuk_app.views.main.ui.theme.br2
 import lapuk_app.views.main.ui.theme.br3
+import lapuk_app.views.main.ui.theme.br4
 import lapuk_app.views.main.ui.theme.br5
-import java.util.concurrent.CountDownLatch
 
-@OptIn(ExperimentalCoroutinesApi::class)
 @Composable
 fun SavePreviewDialog(
     imageBitmap: Bitmap, onDismiss: (Boolean) -> Unit
 ) {
     val context = LocalContext.current
-    val loadingLatch = CountDownLatch(1)
     val imageResult = remember { mutableStateOf<Bitmap?>(imageBitmap) }
     val isLoading = remember { mutableStateOf(true) }
+    val isAnalysisSuccessful = remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         try {
             withTimeout(30000) { // throws error if not finished in half minute
                 requestAnalysis(encodedString = encodeBitmap(imageBitmap), callback = { result ->
-                    imageResult.value = decodeToBitmap(result)
-                    isLoading.value = false
+                    try {
+                        imageResult.value = decodeToBitmap(result)
+                        isAnalysisSuccessful.value = true
+                    } catch (e: Exception) {
+                        isAnalysisSuccessful.value = false
+                    } finally {
+                        isLoading.value = false
+                    }
                 })
             }
         } catch (e: Exception) { // show error message, if any. reset imageResult to original image
             Toast.makeText(
                 context, "Image Analysis Error: ${e.message}", Toast.LENGTH_SHORT
             ).show()
+
             isLoading.value = false
+            isAnalysisSuccessful.value = false
+
             imageResult.value = imageBitmap
         }
     }
 
-    if (isLoading.value) LoadingDialog(onDismiss = {
-        loadingLatch.countDown() // Cancel the image analysis
-        onDismiss(false)
-        Toast.makeText(
-            context, "Image Analysis Cancelled.", Toast.LENGTH_SHORT
-        ).show()
-    })
+    if (isLoading.value) {
+        LoadingDialog(onDismiss = {
+            onDismiss(false)
+            Toast.makeText(
+                context, "Image Analysis Cancelled.", Toast.LENGTH_SHORT
+            ).show()
+        })
+    }
 
-    if (!isLoading.value) Dialog(onDismissRequest = { onDismiss(false) }) {
-        Card(
-            modifier = Modifier
-                .fillMaxHeight(.73f)
-                .requiredWidth(400.dp)
-                .padding(20.dp),
-            colors = CardDefaults.cardColors(containerColor = br1),
-            shape = RoundedCornerShape(15.dp)
-        ) {
-            Column {
-                Image(
-                    imageResult.value!!.asImageBitmap(),
-                    contentDescription = "Captured Image",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(.8f)
-                        .padding(15.dp),
-                    contentScale = ContentScale.Fit
-                )
+    if (!isLoading.value) {
+        Dialog(onDismissRequest = { onDismiss(false) }) {
+            Card(
+                modifier = Modifier
+                    .fillMaxHeight(.73f)
+                    .requiredWidth(400.dp)
+                    .padding(20.dp),
+                colors = CardDefaults.cardColors(containerColor = br1),
+                shape = RoundedCornerShape(15.dp)
+            ) {
+                Column {
+                    Image(
+                        imageResult.value!!.asImageBitmap(),
+                        contentDescription = "Captured Image",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(.8f)
+                            .padding(15.dp),
+                        contentScale = ContentScale.Fit
+                    )
 
-                Text(
-                    "Save this classification?",
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                )
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(15.dp),
-                    horizontalArrangement = Arrangement.SpaceAround
-                ) {
-                    // Retake button
-                    IconButton(modifier = Modifier
-                        .height(50.dp)
-                        .width(100.dp)
-                        .border(2.dp, br5, shape = RoundedCornerShape(10.dp))
-                        .shadow(2.dp, shape = RoundedCornerShape(10.dp))
-                        .background(br3, shape = RoundedCornerShape(10.dp)), onClick = {
+                    Text(
+                        "Save this classification?",
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(15.dp),
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        // Retake button
+                        IconButton(modifier = Modifier
+                            .height(50.dp)
+                            .width(100.dp)
+                            .border(2.dp, br5, shape = RoundedCornerShape(10.dp))
+                            .shadow(2.dp, shape = RoundedCornerShape(10.dp))
+                            .background(br3, shape = RoundedCornerShape(10.dp)), onClick = {
+                            onDismiss(false)
+                        }) {
+                            Text(
+                                text = "Retake", modifier = Modifier.padding(3.dp)
+                            )
+                        }
 
-                        onDismiss(false)
-                    }) {
-                        Text(
-                            text = "Retake", modifier = Modifier.padding(3.dp)
-                        )
-                    }
-
-                    // Save button
-                    IconButton(modifier = Modifier
-                        .height(50.dp)
-                        .width(100.dp)
-                        .border(2.dp, br5, shape = RoundedCornerShape(10.dp))
-                        .shadow(2.dp, shape = RoundedCornerShape(10.dp))
-                        .background(br3, shape = RoundedCornerShape(10.dp)), onClick = {
-
-                        onDismiss(false)
-                    }) {
-                        Text(
-                            text = "Save", modifier = Modifier.padding(3.dp)
-                        )
+                        // Save button
+                        IconButton(
+                            modifier = Modifier
+                                .height(50.dp)
+                                .width(100.dp)
+                                .border(
+                                    2.dp,
+                                    if (isAnalysisSuccessful.value) br5 else br4,
+                                    shape = RoundedCornerShape(10.dp)
+                                )
+                                .shadow(2.dp, shape = RoundedCornerShape(10.dp))
+                                .background(
+                                    if (isAnalysisSuccessful.value) br3 else br2,
+                                    shape = RoundedCornerShape(10.dp)
+                                ),
+                            enabled = isAnalysisSuccessful.value,
+                            onClick = {
+                                onDismiss(false)
+                            }
+                        ) {
+                            Text(
+                                text = "Save",
+                                modifier = Modifier.padding(3.dp),
+                                color = if (isAnalysisSuccessful.value) Color.Black else br5
+                            )
+                        }
                     }
                 }
             }
